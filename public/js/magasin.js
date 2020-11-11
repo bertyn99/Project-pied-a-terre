2b65ead147b053848c59277fea9c6be692820158
bertyn99 <bboulikou@gmail.com>
2020-11-11 17:52:22 +0100

    ajout du crud Magasin

--- public/js/magasin.js
+++ public/js/magasin.js
@@ -1,6 +1,25 @@
-class magasin {
+import Commentaire from "./Commentaire";
+class Magasin {
     constructor(nom, addresse) {
         this.nom = nom;
         this.addresse = addresse;
+        this.commentaire = []
     }
+
+    loadCommentaire() {
+
+    }
+
+    pinMagasin() {//pin du magasin sur la map
+
+    }
+
+    showMagasin() {
+
+    }
+
+    getInfoMagasin() {
+
+    }
+
 }
