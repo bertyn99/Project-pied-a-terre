804af0da1f79e3a6eda2dbc17664128b86bc1d37
bertyn99 <bboulikou@gmail.com>
2020-11-11 17:40:58 +0100

    ajout de la class user

---
+++ public/js/users.js
@@ -0,0 +1,7 @@
+class users {
+    constructor(name, position) {
+        this.name = name;
+        this.position = position;
+        this.favoris = []//stock l'id des magasin qu'il aime
+    }
+}
