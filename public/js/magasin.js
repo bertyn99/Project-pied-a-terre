527be5fd4dead99eee927fd018e9f81e8cba92a6
bertyn99 <bboulikou@gmail.com>
2020-11-12 01:22:56 +0100

    ajout de getMagasinItem

--- public/js/magasin.js
+++ public/js/magasin.js
@@ -18,7 +18,16 @@ module.exports = class Magasin {
 
     }
 
-    getDetails() {
+    getDetails() {//recup les info du magasin par l'api google  et remplir les valeur de l'object
+        this.nom
+        this.addresse
+        this.commentaire
+    }
+
+    getMagasinItem() {
+        const item = document.querySelector("#card").content.cloneNode(true);
+        item.querySelector(".card-footer-title").textContent = this.nom;
+        item.querySelector(".card-footer-address").textContent = this.addresse;
 
     }
 
