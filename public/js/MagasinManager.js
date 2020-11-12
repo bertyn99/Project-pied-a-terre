ea2ef9d2e5b16b3e2e6bfd1064fc6647cce69f09
bertyn99 <bboulikou@gmail.com>
2020-11-12 01:23:40 +0100

    ajout de la class Magasin Manager

--- public/js/MagasinManager.js
+++ public/js/MagasinManager.js
@@ -2,9 +2,33 @@ module.exports = class MagasinManager {
     constructor() {
         this.magasins = [];
         this._viewContainer = document.querySelector("main");
-        history.replaceState({ view: this.getViewNameFromLocation() }, "", window.location.toString());
-        window.addEventListener("popstate", (e) => {
-            this.render(e.state.view);
-        });
+    }
+
+    set view(dom) {
+        this._viewContainer.innerHTML = "";
+        this._viewContainer.appendChild(dom);
+    }
+
+    getMagasinList() {//charge la liste de magasin
+
+    }
+
+    render(vue) {
+        for (const store of this.magasins) {
+            if (vue == store.name) {
+                this.renderMagasinView(store);
+            }
+        }
+    }
+
+    renderMagasinView(store) {//affiche la vue details d'un magasin
+
+    }
+
+    renderMagasinListView() {
+        const listView = document.querySelector("#card-list").content.cloneNode(true);
+        for (let store of this.magasins) {
+            listView.querySelector(".list-container").appendChild(store.getMagasinItem());
+        }
     }
 }
