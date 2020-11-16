71470a7bd25623eeb1da9340ba6f7bc0ad73f41f
bertyn99 <bboulikou@gmail.com>
2020-11-16 19:30:25 +0100

    ajout de loadScriptMap dans Mapmanager

--- public/js/MapManager.js
+++ public/js/MapManager.js
@@ -1,49 +1,22 @@
-module.exports = class MapManager {
+export default class MapManager {
     constructor() {
-        this.magasins = [];
-        this._viewContainer = document.querySelector("main");
     }
 
-    set view(dom) {
-        this._viewContainer.innerHTML = "";
-        this._viewContainer.appendChild(dom);
-    }
 
-    load() {
-        let userCoord = { lat: -25.363, lng: 131.044 };
-        let map = new google.maps.Map(document.querySelector('.carte'), {
-            center: userCoord,
-            zoom: 10
-        });
+    loadScriptMap(src) {
+        return new Promise((resolve, reject) => {
+            let script = document.createElement('script');
+            script.src = src;
+
+            script.onload = () => resolve(script);
+
+            document.head.append(script);
+        })
 
     }
 
-    showMap() {
-        const mapView = document.querySelector("#tempmap").content.cloneNode(true);
 
-        this.view = mapView;
-    }
 
-    getMagasinList() {//charge la liste de magasin
 
-    }
 
-    render(vue) {
-        for (const store of this.magasins) {
-            if (vue == store.name) {
-                this.renderMagasinView(store);
-            }
-        }
-    }
-
-    renderMagasinView(store) {//affiche la vue details d'un magasin
-
-    }
-
-    renderMagasinListView() {
-        const listView = document.querySelector("#card-list").content.cloneNode(true);
-        for (let store of this.magasins) {
-            listView.querySelector(".list-container").appendChild(store.getMagasinItem());
-        }
-    }
 }
